6dc89c3ab84a6f5d9a280b29697f8c5a5b102195
Leonardo Federmann <leofedermann@gmail.com>
2021-06-24 19:34:20 -0300

    atualizei o README e fiz alguns ajustes

--- luby-react-test/src/TESTE DE LÓGICA/questao-4.js
+++
@@ -1,8 +0,0 @@
-export const arrayToObject = (list) => {
-    let result = {}
-    list.forEach((item) => {
-        const object = { ...result, [item[0]]: item[1] }
-        result = object
-    })
-    return result
-}
--- luby-react-test/src/TESTE DE LÓGICA/questao-5.js
+++
@@ -1,8 +0,0 @@
-//Assumo que, conforme o exemplo, a função receberá dois itens após o array de entrada.
-
-export const removeFromArray = (list, itemA, itemB) => {
-    const cleanList = list.filter((item) => {
-        return item !== itemA && item !== itemB
-    })
-    return cleanList
-}
